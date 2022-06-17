9452fa8f1284936e8cc45e04146c233681b7593b
s1928511 <j.konijn@student.utwente.nl>
2022-06-17 10:24:04 +0200

    versioning

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "[ERROR] Installer not tested & done yet"
 
-sudo apt-get update && sudo apt-get upgrade
 
 sudo apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python3 python3-dev swig zlib1g-dev -y
 
@@ -20,8 +19,38 @@ rm -r nfdump
 
 echo "[INFO] Trying to install ZEEK"
 
-echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
-curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+
+sudo apt update
+sudo apt install zeek-lts
+
+. /etc/os-release
+
+echo -n "Ubuntu version: $VERSION_ID"
+
+case $VERSION_ID in
+
+  22.04)
+
+    echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
+    curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+    ;;
+
+  21.10)
+    echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_21.10/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
+    curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_21.10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+    ;;
+
+  20.04)
+    echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
+    curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+    ;;
+
+  18.04)
+    echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
+    curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_18.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+    ;;
+esac
+
 sudo apt update
 sudo apt install zeek-lts
 
