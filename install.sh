db47a7a26d0439e93d5632bafd2d5af4970e4e28
s1928511 <j.konijn@student.utwente.nl>
2022-06-17 10:00:11 +0200

    installer update

--- install.sh
+++ install.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 echo "[ERROR] Installer not tested & done yet"
 
+sudo apt-get update && sudo apt-get upgrade
 
-exit 0
-
-sudo apt-get install flex
-sudo apt-get install libpcap-dev
+sudo apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python3 python3-dev swig zlib1g-dev -y
 
 git clone https://github.com/phaag/nfdump.git
 cd nfdump
-chmod u+x autogen.sh && ./autogen.sh
+
+./autogen.sh
 ./configure --enable-sflow --enable-readpcap --enable-nfpcapd
 make
+
 sudo make install
+sudo ldconfig
+
+echo "[INFO] Cleaning up"
+rm -r nfdump
+
+echo "[INFO] Trying to install ZEEK"
+
+echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
+curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+sudo apt update
+sudo apt install zeek-lts
+
